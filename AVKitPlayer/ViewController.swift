eb4306f3345488b7c04fcfcd473d750c2bb96eb8
shanth kumar <shanth923@gmail.com>
2020-01-02 12:49:07 +0530

    added

--- AVKitPlayer/ViewController.swift
+++ AVKitPlayer/ViewController.swift
@@ -7,12 +7,84 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    var audioPlayer:AVAudioPlayer = AVAudioPlayer()
+    
+  
+    
+    
+    
+    @IBAction func action(_ sender: UIButton) {
+    
+    if let path = Bundle.main.path(forResource: "car", ofType: "mp4")
+    
+    {
+        
+        let video = AVPlayer(url: URL(fileURLWithPath: path))
+        
+        let videoPlayer = AVPlayerViewController()
+        
+        videoPlayer.player = video
+        
+        
+        present(videoPlayer, animated: true) {
+            video.play()
+        }
+        
+        
+        
+        
+        }
+    
+    
+    
+    
+    
+    
+    }
+    
+    
+    @IBAction func pause(_ sender: Any) {
+        
+        audioPlayer.pause()
+        
+    }
+    
+    @IBAction func paly(_ sender: Any) {
+        
+        audioPlayer.play()
+        
+    }
+    
+    @IBAction func replay(_ sender: Any) {
+        
+        audioPlayer.currentTime = 0
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        do{
+            
+            let path = Bundle.main.path(forResource: "BadBoy", ofType: "mp3")
+            
+            try audioPlayer =   AVAudioPlayer(contentsOf: URL(fileURLWithPath: path!))
+         
+            
+        }catch{
+            
+            print("error")
+        }
+        
+        
+        
     }
 
 
